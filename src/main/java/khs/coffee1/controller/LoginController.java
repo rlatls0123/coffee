022b05ffdd9f53b9ae822f5b5f2d5e0385b8f391
hongs <hongsin6846@gmail.com>
2026-06-02 18:05:22 +0900

    회원가입 화면 추가(@Getmmapping(signup)) User.phone 추가

--- src/main/java/khs/coffee1/controller/LoginController.java
+++ src/main/java/khs/coffee1/controller/LoginController.java
@@ -12,4 +12,9 @@ public class LoginController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 }
--- src/main/java/khs/coffee1/domain/User.java
+++ src/main/java/khs/coffee1/domain/User.java
@@ -19,6 +19,8 @@ public class User {
     private String name;
     private String loginId;
     private String password;
+    private String phone;
+
 
     @OneToMany(mappedBy = "user")
     private final List<Order> orders = new ArrayList<>();
